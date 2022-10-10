b1a1c1f0ce9c34f10da8b5ebac5762e804ad192d
janvarp <jpitelkov@gmail.com>
2022-10-10 16:08:33 +0300

    array: splice, push, filter, map

--- veebipood/src/pages/Ostukorv.js
+++ veebipood/src/pages/Ostukorv.js
@@ -1,5 +1,36 @@
+import { useState } from "react";
+
 function Ostukorv() {
-    return ( <div>Ostukorv</div> );
+    // const ostukorv = ["Nobe", "Mercedes", "Tesla", "BMW"];
+    const [ostukorv, uuendaOstukorv] = useState(["Nobe", "Mercedes", "Tesla", "BMW"]);
+
+    const kustuta = (jarjekorraNumber) => {
+        ostukorv.splice(jarjekorraNumber,1); // splice - kustutan
+        console.log(ostukorv);
+        uuendaOstukorv(ostukorv.slice()); // slice - teen koopia
+    }
+
+    const lisa = (klikitudToode) => {
+        ostukorv.push(klikitudToode);
+        uuendaOstukorv(ostukorv.slice()); // push - lisa 1 loppu juurde
+    }
+
+    const tyhjenda = () => {
+        uuendaOstukorv([]);
+    }
+
+    return ( 
+    <div>
+        {ostukorv.length === 0 && <div>Ostukorv on tuhi!</div>}
+        { ostukorv.length > 0 && <div>Ostukorvis on {ostukorv.length} toodet</div>}
+        { ostukorv.length > 0 && <button onClick={tyhjenda}>Tuhjenda</button>}
+        {ostukorv.map((element, jarjekorraNumber) =>
+            <div key={jarjekorraNumber}>
+                {element}
+                <button onClick={() => kustuta(jarjekorraNumber)}>x</button>
+                <button onClick={() => lisa(element)}>Lisa</button>
+            </div>)}
+        </div> );
 }
 
 export default Ostukorv;
--- veebipood/src/pages/Poed.js
+++ veebipood/src/pages/Poed.js
@@ -2,10 +2,11 @@
 // s6num = useState("Tere")   ---> a) "Edukalt lisatud"  b) "MIdagi on puudu"
 // array = useState(["uks", "kaks", "kolm"])  ---> ["uks","kaks"]
 
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 function Poed() {
     const [poed, uuendaPoed] = useState(["Kristiine","Mustam2e","Kesklinn","6ism2e","P6hja-Tallinn","Lasnam2e"])
+    const poodRef = useRef();
 
     const sortAZ = () => {
         poed.sort();
@@ -17,21 +18,54 @@ function Poed() {
         uuendaPoed(poed.slice());
     }
 
-    return ( <div>
+    const kustuta = (index) => {
+    poed.splice(index,1);   // esimene on mitmendat kustutan, koma jarel teine mitu tukki kustutan
+    uuendaPoed(poed.slice());   // HTMLi muutmiseks (eraldi kasklkus - muuda HTMLi, sest React on effektiivne ja React ei lahe iga muutuja peale HTMLi ka muutma)
+                            // kask HTMLi muutmiseks on useState parempoolen funktsioon const [vasak,parem] = useState(algavaartus)
+}
+
+    const lisa = () => {
+        poed.push(poodRef.current.value);
+        uuendaPoed(poed.slice());
+    }
+
+    const filtreeri = () => {
+        const vastus = poed.filter(element => element.includes("m2e"));
+        uuendaPoed(vastus);
+    }
+
+    const muudaK6iki = () => {
+        const vastus = poed.map(element => element + "--");
+        uuendaPoed(vastus);
+    }
+
+    return ( 
+    <div>
+        <label>Uus pood</label> <br />
+        <input ref={poodRef} type="text" /> <br />
+        <button onClick={lisa}>Sisesta</button> <br />
+
         <button onClick={sortAZ}>Sorteeri A-Z</button>
         <button onClick={sortZA}>Sorteeri Z-A</button>
+        <button onClick={filtreeri}>Jata alles "m2e" sisaldavad</button>
+        <button onClick={muudaK6iki}>Lisa taiendavad -- loppu</button>
 
         {/* .map() on funktsionaalsus, mis kehtib alati array-dele
          map on ukshaaval iga elemendi v]tmine ja tema muutmine html-ks*/}
+        <div>{["","",""].map((e,i) => <div key={i}>{i}. Tere</div>)}</div>
+        <div>{poed.map((pood, index) =>
+        <div key={pood}>
+            {pood}
+             <button onClick={() =>kustuta(index)}>x</button>
+             </div>)}
+        </div>
 
-        <div>{poed.map(pood => <div>{poed}</div>)}</div>
-
-        <div>Kristiine</div>
-        <div>Mustam2e</div>
-        <div>Kesklinn</div>
-        <div>6ism2e</div>
-        <div>P6hja-Tallinn</div>
-        <div>Lasnam2e</div>
+        <div key={"Kristiine"}>Kristiine <button onClick={() => kustuta(0)}>x</button> </div>
+        <div key={"Mustam2e"}>Mustam2e <button onClick={() => kustuta(1)}>x</button> </div>
+        <div key={"Kesklinn"}>Kesklinn <button onClick={() => kustuta(2)}>x</button> </div>
+        <div key={"6ism2e"}>6ism2e <button onClick={() => kustuta(3)}>x</button> </div>
+        <div key={"P6hja-Tallinn"}>P6hja-Tallinn <button onClick={() => kustuta(4)}>x</button> </div>
+        <div key={"L2snam2e"}>Lasnam2e <button onClick={() => kustuta(5)}>x</button> </div>
     </div>  );
 }
 
