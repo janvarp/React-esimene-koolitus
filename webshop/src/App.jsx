4ca30df73a493901b1fd5e7752c8788b6204e7ee
janvarp <jpitelkov@gmail.com>
2022-10-24 16:08:30 +0300

    t6lge

---
+++ webshop/src/App.jsx
@@ -0,0 +1,41 @@
+import {Route, Routes } from "react-router-dom"
+import './App.css';
+import Homepage from "./pages/Homepage.jsx";
+import Cart from "./pages/Cart.jsx";
+import AboutUs from "./pages/AboutUs.jsx";
+import Shops from "./pages/Shops.jsx";
+import SingleProduct from "./pages/SingleProduct.jsx";
+import AddProduct from "./pages/admin/AddProduct.jsx";
+import AdminHome from "./pages/admin/AdminHome.jsx";
+import EditProduct from "./pages/admin/EditProduct.jsx";
+import MaintainCategories from "./pages/admin/MaintainCategories.jsx";
+import MaintainProducts from "./pages/admin/MaintainProducts.jsx";
+import MaintainShops from "./pages/admin/MaintainShops.jsx";
+import NavigationBar from "./components/NavigationBar";
+
+
+
+
+function App() {
+  return (
+    <div>
+     <NavigationBar />
+   
+      <Routes>
+        <Route path="" element={ <Homepage/>}/>
+        <Route path="cart" element={ <Cart/>}/>
+        <Route path="about-us" element={ <AboutUs/>}/>
+        <Route path="shops" element={ <Shops/>}/>
+        <Route path="product" element={ <SingleProduct/>}/>
+        <Route path="admin/add-product" element={ <AddProduct/>}/>
+        <Route path="admin" element={ <AdminHome/>}/>
+        <Route path="admin/edit-product" element={ <EditProduct/>}/>
+        <Route path="admin/maintain-categories" element={ <MaintainCategories/>}/>
+        <Route path="admin/maintain-products" element={ <MaintainProducts/>}/>
+        <Route path="admin/maintain-shops" element={ <MaintainShops/>}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
