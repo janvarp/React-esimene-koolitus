0834499d25d1706bffb4fb3bc1c1a4c437dcf02d
janvarp <jpitelkov@gmail.com>
2022-11-14 16:28:05 +0200

    webshop cart update

--- webshop/src/pages/Cart.jsx
+++ webshop/src/pages/Cart.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useMemo, useState } from "react";
 import config from "../data/config.json"
+import styles from "../css/Cart.module.css";
 
 function Cart() {
     const [cart, setCart] = useState( [] );
@@ -66,22 +67,53 @@ const emptyCart = () => {
     
 }
 
+const pay = () => {
+    // enne maksma hakkamist ma salvestan tellimuse andmebaasi
+
+    const paymentData = {
+            "api_username": "92ddcfab96e34a5f",         // kasutajanimi, mis peab uhtima headersis oleva kasutajanimega
+            "account_name": "EUR3D1",       // konto nimi
+            "amount": calculateCartSum(),              // kogusumma
+            "order_reference": Math.random()*999999,   // tellimuse nr, error kui see tellimuse nr on juba tasutud
+            "nonce": "a9124858oihhloltere902" + Math.random()*999999 + new Date(),      // turvaelement, iga p'ring peab olmea unikaalne
+            "timestamp": new Date(),       // turvaelement, ajatempel
+            "customer_url": "https://nepla.web.app"         // aadress, kuhu teda hiljem tagasi suunatakse
+    }
+    const headersData = {
+        "Authorization": "Basic OTJkZGNmYWI5NmUzNGE1Zjo4Y2QxOWU5OWU5YzJjMjA4ZWU1NjNhYmY3ZDBlNGRhZA==",
+        "Content-Type": "application/json"
+    }
+    fetch("https://igw-demo.every-pay.com/api/v4/payments/oneoff",{
+        "method": "POST",
+        "body": JSON.stringify(paymentData),
+        "headers": headersData
+    }).then(res => res.json())
+      .then(json => window.location.href = json.payment_link)
+}
+
     return ( 
     <div>
-    {cart.length === 0 && <div>Cart is empty!</div>}
-        { cart.length > 0 && <div>{cart.length} products in cart</div>}
-        { cart.length > 0 && <button onClick={emptyCart}>Empty</button>}
+        <div className={styles.cart__top}>
+            <button onClick={emptyCart}>Tuhjenda</button>
+            <div>Toodete koguarv {cart.length} tk</div>
+        </div>
+
         {cart.map((element, productClicked) =>
-            <div key={productClicked}>
-                <img src={element.product.image} alt="" />
-                <div>{element.product.name}</div>
-                <button onClick={() => decreaseQuantity(productClicked)} >-</button>
-                <div>{element.quantity}</div>
-                <button onClick={() => increaseQuantity(productClicked)} >+</button>
-                <div>{(element.product.price * element.quantity).toFixed(2)}</div>
-                <button onClick={() => removeFromCart(productClicked)}>x</button>
+            <div key={productClicked} className={styles.product}>
+                <img className={styles.image} src={element.product.image} alt="" />
+                <div className={styles.name}>{element.product.name}</div>
+                <div className={styles.price}>{element.product.price.toFixed(2)} $</div>
+                <div className={styles.quantity}>
+                    <img className={styles.button} onClick={() => decreaseQuantity(productClicked)} src={require("../images/minus.png")} alt="" />
+                    <div>{element.quantity} tk</div>
+                    <img className={styles.button} onClick={() => increaseQuantity(productClicked)} src={require("../images/plus.png")} alt="" />
+                </div>
+                <div className={styles.sum}>{(element.product.price * element.quantity).toFixed(2)} $</div>
+                <img className={styles.button} onClick={() => removeFromCart(productClicked)} src={require("../images/remove.png")} alt="" />
+
+        </div>)}
                 
-            </div>)}
+            <div className={styles.cart__bottom}>
             <select>
                 {parcelMachines
                 .filter(element => element.A0_NAME === "EE" && element.ZIP !== "96331")
@@ -89,9 +121,12 @@ const emptyCart = () => {
                  <option>{element.NAME}</option>)}
             </select>
 
-            <div>Wholesum: {calculateCartSum()} €</div>
-    </div>
-     );
+            <div>Kokku: {calculateCartSum()} €</div>
+            <button onClick={pay}>Maksma</button>
+        </div>
+
+    </div>);
+
 }
 
 export default Cart;
